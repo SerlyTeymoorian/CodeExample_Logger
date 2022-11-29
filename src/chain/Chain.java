408d60dc2008bf7fd8a42f2c3774ec27c669ecc2
Serly Teymoorian <98438828+SerlyTeymoorian@users.noreply.github.com>
2022-11-28 17:17:07 -0800

    update

--- src/chain/Chain.java
+++ src/chain/Chain.java
@@ -7,18 +7,22 @@ import handlers.Logger;
 
 public class Chain {
 	
+	//root logger 
 	private Logger l1;  
 	
+	//setting up the chain 
 	public Chain() {
 		l1 = new ConsoleBasedLogger(); 
 		Logger l2 = new ErrorBasedLogger();
 		Logger l3 = new DebugBasedLogger(); 
 		
+		//handler pointing to the next handler 
 		l1.setNextLevelLogger(l2);
 		l2.setNextLevelLogger(l3);
 		
 	}
 	
+	//return root logger 
 	public Logger getLogger() {
 		return l1; 
 	}
