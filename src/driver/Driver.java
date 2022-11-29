8b58061150c7ea78d042be329ba976afa58d17ed
Serly Teymoorian <98438828+SerlyTeymoorian@users.noreply.github.com>
2022-11-28 17:17:38 -0800

    update

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -7,8 +7,10 @@ public class Driver {
 	
 	public static void main(String[] args) {
 		
+		//create a chain of handlers 
 		Chain chainOfLoggers = new Chain(); 
 		
+		//handle the request by the handlers 
 		chainOfLoggers.getLogger().logMessage(new LoggerRequest("ErrorInfo"), "An error occured now ");
 	}
 
